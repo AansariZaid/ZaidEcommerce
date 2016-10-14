948ea148d2a7d154292e53191171c228566c2ccf
AansariZaid <zaid3891@gmail.com>
2016-10-15 00:13:42 +0530

    commited and uploaded on 14 october

---
+++ Securitronic/src/main/java/com/securitronic/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.securitronic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class HelloWorldController {
+
+	@RequestMapping(value= "/",method = RequestMethod.GET)
+	public String sayHello(ModelMap model) {
+		model.addAttribute("greeting", "Hello World from my First Project");
+		return "test";
+	}
+
+	@RequestMapping(value = "/HelloAgain", method = RequestMethod.GET)
+	public String sayHelloAgain(ModelMap model) {
+		model.addAttribute("greeting", "Hello world Once Again");
+		return "test";
+	}
+	// public ModelAndView mvc = new ModelAndView("/helloagain");
+
+}
