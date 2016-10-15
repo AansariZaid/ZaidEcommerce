92a17c06e65011f9bdcba6beccbd51cbf9200cc2
AansariZaid <zaid3891@gmail.com>
2016-10-15 18:38:10 +0530

    Commited on 15 october with carousel,login page and register page

--- Securitronic/src/main/java/com/securitronic/HelloWorldController.java
+++ Securitronic/src/main/java/com/securitronic/HelloWorldController.java
@@ -1,25 +1,61 @@
 package com.securitronic;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 
 public class HelloWorldController {
 
-	@RequestMapping(value= "/",method = RequestMethod.GET)
-	public String sayHello(ModelMap model) {
-		model.addAttribute("greeting", "Hello World from my First Project");
-		return "test";
+	/*
+	 * @RequestMapping(value= "/",method = RequestMethod.GET) public String
+	 * sayHello(ModelMap model) { model.addAttribute("greeting",
+	 * "Hello World from my First Project"); return "landingpage"; } //this is
+	 * one way of calling a page
+	 * 
+	 * @RequestMapping(value= "/home",method = RequestMethod.GET)
+	 * 
+	 * public String sayhome(ModelMap model) { model.addAttribute("greeting",
+	 * "Hello World from my First Project"); return "home"; }
+	 */
+
+	// we can also use modelandview to do the same calling.
+
+	@RequestMapping(value = "/")
+	public ModelAndView landingpage() {
+		ModelAndView mv = new ModelAndView("landingpage");
+		return mv;
 	}
 
-	@RequestMapping(value = "/HelloAgain", method = RequestMethod.GET)
-	public String sayHelloAgain(ModelMap model) {
-		model.addAttribute("greeting", "Hello world Once Again");
-		return "test";
-	}
-	// public ModelAndView mvc = new ModelAndView("/helloagain");
+	@RequestMapping(value = "/home")
+	public ModelAndView home() {
+		ModelAndView mv = new ModelAndView("home");
+		return mv;
 
+	}
+
+	@RequestMapping(value = "aboutus")
+	public ModelAndView aboutUs() {
+		ModelAndView mv = new ModelAndView("aboutus");
+		return mv;
+	}
+
+	@RequestMapping(value = "contactus")
+	public ModelAndView contactus() {
+		ModelAndView mv = new ModelAndView("contactus");
+		return mv;
+	}
+
+	@RequestMapping(value = "login")
+	public ModelAndView login() {
+		ModelAndView mv = new ModelAndView("login");
+		return mv;
+	}
+
+	@RequestMapping(value = "register")
+	public ModelAndView register() {
+		ModelAndView mv = new ModelAndView("register");
+		return mv;
+	}
 }
---
+++ Securitronic/src/main/java/com/securitronic/PageController.java
@@ -0,0 +1,5 @@
+package com.securitronic;
+
+public class PageController {
+
+}
